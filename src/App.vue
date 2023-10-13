ee2dda208f430d215bd6f30fbb21f258843e60b5
MateoLot <mateo.lotero@udea.edu.co>
2023-10-12 22:23:14 -0500

    Cambios y eliminar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CalcVida></CalcVida>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalcVida from './components/CalcVida.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalcVida
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+  margin-left: 0%;
+  margin-right: 0%;
+}
 #app {
+  background-color: cadetblue;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
+  margin-top: 8%;
+  margin-bottom: 5%;
+  height: 100%;
+  width: 100%;
+}
+#fam, #her, #gov, #sal, #empre, #par, #hij, #deud {
+  margin-top: 0.5%;
+  margin-bottom: 0.5%;
 }
 </style>
---
+++ src/components/CalcVida.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <h1>Calculadora de la Vida</h1>
+        <label for="edad">Edad:</label>
+        <input type="number" v-model="edad" @input="calcularDinero">
+        <div id="fam">
+            <label for="familia">Familia:</label>
+            <input type="number" v-model="familia" @input="calcularDinero">
+        </div>
+        <div id="her">
+            <label for="herencia">Herencia Familiar:</label>
+            <input type="number" v-model="herencia" @input="calcularDinero">
+        </div>
+        <div id="gov">
+            <label for="gobierno">Gobierno:</label>
+            <input type="number" v-model="gobierno" @input="calcularDinero">
+        </div>
+        <div id="sal">
+            <label for="salario">Salario:</label>
+            <input type="number" v-model="salario" @input="calcularDinero">
+        </div>
+        <div id="empre">
+            <label for="emprendimiento">Emprendimiento:</label>
+            <input type="number" v-model="emprendimiento" @input="calcularDinero">
+        </div>
+        <div id="par">
+            <label for="pareja">Pareja:</label>
+            <input type="number" v-model="pareja" @input="calcularDinero">
+        </div>
+        <div id="hij">
+            <label for="hijos">Hijos:</label>
+            <input type="number" v-model="hijos" @input="calcularDinero">
+        </div>
+        <div id="deud">
+            <label for="deudas">Deudas:</label>
+            <input type="number" v-model="deudas" @input="calcularDinero">
+        </div>
+        <p>Dinero acumulado: {{ dinero }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CalcVida',
+    data() {
+        return {
+            edad: 0,
+            familia: 0,
+            herencia: 0,
+            gobierno: 0,
+            salario: 0,
+            emprendimiento: 0,
+            pareja: 0,
+            hijos: 0,
+            deudas: 0,
+            dinero: 0
+        };
+    },
+    methods: {
+        calcularDinero() {
+            this.dinero = (this.familia + this.herencia + this.gobierno + this.salario + this.emprendimiento - this.pareja - this.hijos - this.deudas);
+        }
+    }
+};
+</script>
